bf86344cbdc616527862376b1e209b978d3f96ea
KashishRanaa <kashishranaa@gmail.com>
2024-04-10 18:25:52 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-hyphen_e_dot=" -e . " 
+hyphen_e_dot='-e .' 
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -18,12 +18,9 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='mlproject',
-    version='23.3.1',
+    version='0.0.1',
     author='Kashish',
     author_email='kashishranaa@gmail.com',
     packages= find_packages(),
     install_requires= get_requirements('requirements.txt')
-
-
-
 )
